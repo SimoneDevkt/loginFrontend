84ccf9e401c65258601d6bef900c6bf746f50318
Simone <simonep97@gmail.com>
2023-04-02 11:18:08 +0200

    test deploy netlify

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	export const prerender = true;
+</script>
+
+<form name="form-example" data-netlify="true">
+    <input type="hidden" name="form-name" />
+    <label for="name">Name</label>
+    <input name="name" id="name" required placeholder="Name" type="text" />
+    <label for="email">Email</label>
+    <input name="email" id="email" required placeholder="Email" type="email" />
+    <label for="message">Message</label>
+    <input name="message" id="message" required placeholder="Message" type="text" />
+    <input type="submit" value="Submit" />
+</form>
